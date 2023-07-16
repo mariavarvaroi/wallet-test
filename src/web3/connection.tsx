685f0e88e354fdca854ef73d5f476ef72642230d
maria <maria@chorus.one>
2023-07-16 18:06:17 +0100

    add privatenet config, update package

--- src/web3/connection.tsx
+++ src/web3/connection.tsx
@@ -5,16 +5,37 @@ import theme, { PALETTE } from './theme'
 import { WALLETCONNECT_CONFIG } from "./config/walletconnect";
 import { ROOT_CONFIG } from "./config/root";
 import { CONNECTORS_CONFIG } from "./config/connectors";
+import { Chain } from "@wagmi/core"
 
-export type SupportedChainIds = 1 | 5
-const availableChains = [mainnet, goerli];
+const privatenet: Chain = {
+    id: 4242,
+    network: "minimal",
+    name: "Hopper privatenet",
+    nativeCurrency: { name: "Hopper Ether", symbol: "ETH", decimals: 18 },
+    rpcUrls: {
+        default: {
+            http: ["http://localhost:8545"],
+        },
+        public: {
+            http: ["http://localhost:8545"],
+        },
+        privatenet: {
+            http: ["http://localhost:8545"],
+        },
+    },
+    contracts: {},
+    testnet: true,
+};
 
-export const pstlModalConfig: PstlWeb3ModalProps<1 | 5> = {
+export type SupportedChainIds = 1 | 5 | 4242;
+const availableChains = [mainnet, goerli, privatenet];
+
+export const pstlModalConfig: PstlWeb3ModalProps<1 | 5 | 4242> = {
     appName: 'CHORUS MODAL TEST',
     chains: availableChains,
     connectors: CONNECTORS_CONFIG,
     modals: {
         root: ROOT_CONFIG,
         walletConnect: WALLETCONNECT_CONFIG
-    }
+    },
 }
