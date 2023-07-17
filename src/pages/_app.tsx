6405a6b014bc1d1849a3d0dac1ea106343999428
w3stside <david.sato64@gmail.com>
2023-07-17 11:58:20 +0200

    fixes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,16 @@ import type { AppProps } from "next/app";
 import AppWithWeb3Access from "../components/AppWithWeb3Access";
 import { pstlModalConfig } from "../web3/connection";
 import { PstlW3Providers } from "@past3lle/web3-modal";
-import { PstlHooksProvider } from "@past3lle/hooks";
+import { WindowSizeProvider } from "@past3lle/hooks";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <PstlHooksProvider>
+        <WindowSizeProvider>
             <PstlW3Providers config={pstlModalConfig}>
                 <Component {...pageProps} />
                 <AppWithWeb3Access />
             </PstlW3Providers>
-        </PstlHooksProvider>
+        </WindowSizeProvider>
     )
 }
 
